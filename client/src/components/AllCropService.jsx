5c39e522997c80b7578cfb9d9101ea2e3bf49ad3
Imran <imran.bursagov@gmail.com>
2022-07-27 16:08:53 +0300

    Добавил новые услуги

--- client/src/components/AllCropService.jsx
+++ client/src/components/AllCropService.jsx
@@ -1,47 +1,206 @@
 const AllCropService = () => {
-  return(
+  return (
     <>
-  <li className="services_list-item" data-aos="fade-up" data-order="1">
-    <div className="services_list-item_wrapper d-flex flex-column flex-lg-row justify-content-between max-w-6xl">
-      <div className="media" data-aos="fade-right">
-        <picture>
-          <source
-            data-srcset="https://i9.photo.2gis.com/images/branch/0/30258560055648542_c00d.jpg"
-            srcSet="https://i9.photo.2gis.com/images/branch/0/30258560055648542_c00d.jpg"
-            type="image/webp"
-          />
-          <img
-            className="services-img lazy"
-            data-src="https://i9.photo.2gis.com/images/branch/0/30258560055648542_c00d.jpg"
-            src="https://i9.photo.2gis.com/images/branch/0/30258560055648542_c00d.jpg"
-            alt="media"
-          />
-        </picture>
-      </div>
-      <div className="main ml-0 lg:!ml-16">
-        <h3 className="title" data-aos="fade-up">
-          Причёска
-        </h3>
-        <p className="description" data-aos="fade-left" data-aos-delay="50">
-          A dui aliquam ultrices eros lorem nibh vivamus. Quis aliquam duis pharetra
-          faucibus ultrices volutpat quisque. Convallis nec lectus mi nec eget odio ac
-          tempor. Nunc egestas sed.
-        </p>
-        <div className="pricing d-flex flex-column flex-sm-row m-none lg:m-auto">
-          <span className="pricing_price" data-aos="fade-left" data-aos-delay="100">
-            Барбер —<span className="highlight"> от 1000 ₽</span>
-          </span>
+      <li className="services_list-item" data-aos="fade-up" data-order="1">
+        <div className="services_list-item_wrapper d-flex flex-column flex-lg-row justify-content-between max-w-6xl">
+          <div className="media" data-aos="fade-right">
+            <picture>
+              <source
+                data-srcset="https://html.merku.love/barbercrop/img/gallery/gallery03.webp"
+                srcSet="https://html.merku.love/barbercrop/img/gallery/gallery03.webp"
+                type="image/webp"
+              />
+              <img
+                className="services-img lazy"
+                data-src="https://html.merku.love/barbercrop/img/gallery/gallery03.webp"
+                src="https://html.merku.love/barbercrop/img/gallery/gallery03.webp"
+                alt="media"
+              />
+            </picture>
+          </div>
+          <div className="main ml-0 lg:!ml-16">
+            <h3 className="title" data-aos="fade-up">
+              Стрижка
+            </h3>
+            <p className="description" data-aos="fade-left" data-aos-delay="50">
+              Почему мы любим классику? Потому что она всегда в моде, подходит каждому мужчине и
+              уместна к любому случаю, от приема у английской королевы до визита к стоматологу.
+            </p>
+            <div className="pricing d-flex flex-column flex-sm-row m-none lg:m-auto">
+              <span className="pricing_price" data-aos="fade-left" data-aos-delay="100">
+                Барбер —<span className="highlight"> от 1000 ₽</span>
+              </span>
+            </div>
+            <div className="wrapper" data-aos="fade-up" data-aos-delay="150">
+              <a className="btn theme-element" href="#">
+                Забронировать
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="wrapper" data-aos="fade-up" data-aos-delay="150">
-          <a className="btn theme-element" href="#">
-            Забронировать
-          </a>
+      </li>
+
+      <li className="services_list-item" data-aos="fade-up" data-order="1">
+        <div className="services_list-item_wrapper d-flex flex-column flex-lg-row justify-content-between max-w-6xl">
+          <div className="media" data-aos="fade-right">
+            <picture>
+              <source
+                data-srcset="https://html.merku.love/barbercrop/img/gallery/gallery04.webp"
+                srcSet="https://html.merku.love/barbercrop/img/gallery/gallery04.webp"
+                type="image/webp"
+              />
+              <img
+                className="services-img lazy"
+                data-src="https://html.merku.love/barbercrop/img/gallery/gallery04.webp"
+                src="https://html.merku.love/barbercrop/img/gallery/gallery04.webp"
+                alt="media"
+              />
+            </picture>
+          </div>
+          <div className="main ml-0 lg:!ml-16">
+            <h3 className="title" data-aos="fade-up">
+              Борода и усы
+            </h3>
+            <p className="description" data-aos="fade-left" data-aos-delay="50">
+              Возможно, вы хотите попробовать моделирование бороды — с четкими контурами и
+              окантовкой. Или предпочитаете естественную форму бороды. Вариантов здесь масса, и
+              разобраться в них помогут наши мастера.
+            </p>
+            <div className="pricing d-flex flex-column flex-sm-row m-none lg:m-auto">
+              <span className="pricing_price" data-aos="fade-left" data-aos-delay="100">
+                Барбер —<span className="highlight"> от 400 ₽</span>
+              </span>
+            </div>
+            <div className="wrapper" data-aos="fade-up" data-aos-delay="150">
+              <a className="btn theme-element" href="#">
+                Забронировать
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </li>
+      </li>
+
+      <li className="services_list-item" data-aos="fade-up" data-order="1">
+        <div className="services_list-item_wrapper d-flex flex-column flex-lg-row justify-content-between max-w-6xl">
+          <div className="media" data-aos="fade-right">
+            <picture>
+              <source
+                data-srcset="https://html.merku.love/barbercrop/img/gallery/gallery01.webp"
+                srcSet="https://html.merku.love/barbercrop/img/gallery/gallery01.webp"
+                type="image/webp"
+              />
+              <img
+                className="services-img lazy"
+                data-src="https://html.merku.love/barbercrop/img/gallery/gallery01.webp"
+                src="https://html.merku.love/barbercrop/img/gallery/gallery01.webp"
+                alt="media"
+              />
+            </picture>
+          </div>
+          <div className="main ml-0 lg:!ml-16">
+            <h3 className="title" data-aos="fade-up">
+              Опасная бритва
+            </h3>
+            <p className="description" data-aos="fade-left" data-aos-delay="50">
+              Если в последний раз вы видели лезвие для бритья в руках деда и всю свою жизнь брились
+              обычным станком, мы настоятельно рекомендуем хотя бы раз попробовать опасное бритьё в
+              Eddy cuts. Эта услуга представляет собой настоящую спа-процедуру.
+            </p>
+            <div className="pricing d-flex flex-column flex-sm-row m-none lg:m-auto">
+              <span className="pricing_price" data-aos="fade-left" data-aos-delay="100">
+                Барбер —<span className="highlight"> от 500 ₽</span>
+              </span>
+            </div>
+            <div className="wrapper" data-aos="fade-up" data-aos-delay="150">
+              <a className="btn theme-element" href="#">
+                Забронировать
+              </a>
+            </div>
+          </div>
+        </div>
+      </li>
+
+      <li className="services_list-item" data-aos="fade-up" data-order="1">
+        <div className="services_list-item_wrapper d-flex flex-column flex-lg-row justify-content-between max-w-6xl">
+          <div className="media" data-aos="fade-right">
+            <picture>
+              <source
+                data-srcset="https://html.merku.love/barbercrop/img/gallery/gallery06.webp"
+                srcSet="https://html.merku.love/barbercrop/img/gallery/gallery06.webp"
+                type="image/webp"
+              />
+              <img
+                className="services-img lazy"
+                data-src="https://html.merku.love/barbercrop/img/gallery/gallery06.webp"
+                src="https://html.merku.love/barbercrop/img/gallery/gallery06.webp"
+                alt="media"
+              />
+            </picture>
+          </div>
+          <div className="main ml-0 lg:!ml-16">
+            <h3 className="title" data-aos="fade-up">
+              Стрижка машинкой
+            </h3>
+            <p className="description" data-aos="fade-left" data-aos-delay="50">
+              Стрижка под машинку идёт не всем. Если вы один из таких счастливчиков — поздравляем.
+              А, может, ваш образ жизни не оставляет времени на то, чтобы следить за укладкой волос,
+              или вы просто думаете, что это не мужское дело.
+            </p>
+            <div className="pricing d-flex flex-column flex-sm-row m-none lg:m-auto">
+              <span className="pricing_price" data-aos="fade-left" data-aos-delay="100">
+                Барбер —<span className="highlight"> от 500 ₽</span>
+              </span>
+            </div>
+            <div className="wrapper" data-aos="fade-up" data-aos-delay="150">
+              <a className="btn theme-element" href="#">
+                Забронировать
+              </a>
+            </div>
+          </div>
+        </div>
+      </li>
+
+      <li className="services_list-item" data-aos="fade-up" data-order="1">
+        <div className="services_list-item_wrapper d-flex flex-column flex-lg-row justify-content-between max-w-6xl">
+          <div className="media" data-aos="fade-right">
+            <picture>
+              <source
+                data-srcset="https://html.merku.love/barbercrop/img/gallery/gallery02.webp"
+                srcSet="https://html.merku.love/barbercrop/img/gallery/gallery02.webp"
+                type="image/webp"
+              />
+              <img
+                className="services-img lazy"
+                data-src="https://html.merku.love/barbercrop/img/gallery/gallery02.webp"
+                src="https://html.merku.love/barbercrop/img/gallery/gallery02.webp"
+                alt="media"
+              />
+            </picture>
+          </div>
+          <div className="main ml-0 lg:!ml-16">
+            <h3 className="title" data-aos="fade-up">
+              Укладка
+            </h3>
+            <p className="description" data-aos="fade-left" data-aos-delay="50">
+              С вами такое бывало? Выходите из барбершопа с идеально уложенными волосами, собираете
+              комплименты, но на следующее утро все меняется (и не всегда в лучшую сторону). Дело в
+              том, что любая мужская стрижка держится на хорошем укладочном средстве.
+            </p>
+            <div className="pricing d-flex flex-column flex-sm-row m-none lg:m-auto">
+              <span className="pricing_price" data-aos="fade-left" data-aos-delay="100">
+                Барбер —<span className="highlight"> от 300 ₽</span>
+              </span>
+            </div>
+            <div className="wrapper" data-aos="fade-up" data-aos-delay="150">
+              <a className="btn theme-element" href="#">
+                Забронировать
+              </a>
+            </div>
+          </div>
+        </div>
+      </li>
     </>
-  )
-}
+  );
+};
 
 export default AllCropService;
