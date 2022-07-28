1b3d20eb2e5fcd43c37eabc607b64ea74dfeb3aa
Askhab <87871443+Askhab7777@users.noreply.github.com>
2022-07-28 19:26:09 +0300

    change

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
-import UserRoute from './users.route.js';
-import Router from 'express'
+import UserRoute from "./users.route.js";
+import PostRoute from "./posts.route.js";
+import Router from "express";
 
-const router = new Router()
+const router = new Router();
 
 router.use(UserRoute);
-router.use(PostRoute)
+router.use(PostRoute);
 
 export default router;
