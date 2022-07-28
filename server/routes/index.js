9be2f9291414874d78d8ac4e6c39d9e043205ca6
Askhab <87871443+Askhab7777@users.noreply.github.com>
2022-07-28 18:21:31 +0300

    добавил импорты новых файлов

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
-import UserRoute from './users.routes.js';
+import UserRoute from './users.route.js';
+import PostRoute from './posts.route.js'
 
 import Router from 'express'
 
 const router = new Router()
 
 router.use(UserRoute);
+router.use(PostRoute)
 
 export default router;
